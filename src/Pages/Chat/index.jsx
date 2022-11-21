f9095d6e1e05b7083d86cbe5e1347cc4d47b6403
ranajit.mukherjee <ranajit.mukherjee@glosity.club>
2022-11-21 14:48:12 +0530

    last commit on client

--- src/Pages/Chat/index.jsx
+++ src/Pages/Chat/index.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useStyles } from "./Styles";
 import ChatCard from "../../Components/ChatCard";
 import Navbar from "../../Components/Navbar/Navbar";
 import { pixToRem } from "../../Utils/pixToRem";
+import io from "socket.io-client";
+import { useSelector } from "react-redux";
 
+const ENDPOIENT = "http://localhost:5000";
+
+var socket, selectedChatCompare;
 const Chat = () => {
+  const [socketConnected, setSocketConnected] = useState(false);
+  const user = useSelector((state) => state.auth.user);
+  useEffect(() => {
+    socket = io(ENDPOIENT);
+    socket.emit("setup", user);
+    socket.on("connected", () => setSocketConnected(true));
+  }, []);
+
   const classes = useStyles();
 
   return (
